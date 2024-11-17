9f476f66fd408156e14f2ed9f300a3c0edca34ec
Scruffy pc <cayabyabrr@yahoo.com>
2024-11-16 23:10:03 -0800

    git ignore

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const axios = require("axios");
+
+// check if relaxed
+
+const BASE_URL = "https://discord.com/api/v10"
+
+
+// async function fetch () {
+//     // await const url = 
+// }
+
+function main() {
+
+    console.log("test");
+}
+
+main();
